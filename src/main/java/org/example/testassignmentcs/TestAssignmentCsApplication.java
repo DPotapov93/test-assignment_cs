c97d9e2f25456d834d009a9154885e84beac08fe
Dmytro Potapov <potapovds1993@gmail.com>
2024-05-02 19:21:08 +0300

    first commit

---
+++ src/main/java/org/example/testassignmentcs/TestAssignmentCsApplication.java
@@ -0,0 +1,13 @@
+package org.example.testassignmentcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAssignmentCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testassignmentcs/TestAssignmentCsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testassignmentcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
